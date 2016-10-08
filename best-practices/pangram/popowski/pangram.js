23620df5d9be4304ec818332134bad04731c172a
Tieffer <popowskiluk@gmail.com>
2016-10-08 16:01:10 +0200

    Type letter to check

--- best-practices/pangram/popowski/pangram.js
+++ best-practices/pangram/popowski/pangram.js
@@ -1,43 +1,50 @@
+function checkLetter() {
 
-var array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ,'o', 'p', 'r', 's', 't','u', 'w', 'x', 'y', 'z'];
-
-function getValue(){
-   var value= document.getElementById("pangramInput").value.toLowerCase();
-   
-   return value.split(''); 
-   
+    var letter = document.getElementById("checkLetter").value.toLowerCase();
+    if (letter.length !== 0) {
+        array = letter;
+    } else {
+        array = 'abcdefghijklmnoprstuwxyz';
+    }
+    return array.split('');
 }
 
-function getFormInput(value){    
-  var tmp = [];    
-    for( var i=0; i < array.length ; i++ ){
-           if(value.indexOf(array[i]) === -1) {         
-               tmp.push(array[i]);  
-           } 
+function getValue() {
+   
+    var value = document.getElementById("pangramInput").value.toLowerCase();
+    return value.split('');
+}
+
+function getFormInput(value) {
+   
+    var tmp = [];
+    for (var i = 0; i < array.length; i++) {
+        if (value.indexOf(array[i]) === -1) {
+            tmp.push(array[i]);
+        }
     }
     return tmp;
     console.log(tmp);
 }
 
 
-function isPangram (value){
-  
-    if (value.length===0){
-        
+function isPangram(value) {
+
+    if (value.length === 0) {
         document.getElementById("result").innerHTML = 'To jest pangram';
         document.getElementById("missingLetters").innerHTML = " ";
-    }else{
+    } else {
         document.getElementById("result").innerHTML = 'To nie jest pangram';
         document.getElementById("missingLetters").innerHTML = getFormInput(getValue());
     }
-    
 }
- 
- document.getElementById("pangramChecker").addEventListener('submit', function(event){
-    
-    event.preventDefault(); 
-    isPangram(getFormInput(getValue())); 
+
+document.getElementById("pangramChecker").addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    checkLetter();
+    isPangram(getFormInput(getValue(checkLetter)));
 });
-    
+
 
 
